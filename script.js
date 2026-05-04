43368b155a9ebc62ffd7213b25fd03dce1efcf29
oretanaka <fotor88@gmail.com>
2026-05-04 18:07:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// アイテムデータ
+const itemData = {
+    "item_001": "白",
+    "item_002": "黒",
+    "item_003": "赤"
+};
 
+// 検索処理
+function searchItem() {
+    const id = document.getElementById("itemSearchBox").value;
+    const result = itemData[id] || "見つかりません";
+    document.getElementById("itemResult").innerText = result;
+}
