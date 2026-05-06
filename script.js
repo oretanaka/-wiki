426350224a21bd251f1088913269327fe44f2968
oretanaka <fotor88@gmail.com>
2026-05-06 14:28:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+let data = { items: [], monsters: [] };
+
 fetch("data.json")
-  .then(res => {
-    if (!res.ok) {
-      throw new Error("data.json 読み込み失敗");
-    }
-    return res.json();
-  })
+  .then(res => res.json())
   .then(json => {
     data = json;
-    console.log("OK:", data);
-  })
-  .catch(err => {
-    console.error("エラー:", err);
+    console.log("loaded");
   });
+
+const searchInput = document.getElementById("search");
+const result = document.getElementById("result");
+
+searchInput.addEventListener("input", () => {
+  const keyword = searchInput.value.toLowerCase();
+  result.innerHTML = "";
+
+  if (!data.items) return;
+  if (!keyword) return;
+
+  const items = data.items.filter(i =>
+    i.name.toLowerCase().includes(keyword)
+  );
+
+  const monsters = data.monsters.filter(m =>
+    m.name.toLowerCase().includes(keyword)
+  );
+
+  result.innerHTML += items.map(i => `
+    <div class="card">
+      アイテム: ${i.name}
+    </div>
+  `).join("");
+
+  result.innerHTML += monsters.map(m => `
+    <div class="card">
+      モンスター: ${m.name}
+    </div>
+  `).join("");
+});
